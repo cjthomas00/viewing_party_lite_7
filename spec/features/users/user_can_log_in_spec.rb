33e5db33b84e803a46859089c4b66e6953c697fd
Caleb Thomas <cjthomas00@comcast.net>
2023-04-03 14:40:49 -0600

    test: add sad path test for failed login attempt.

--- spec/features/users/user_can_log_in_spec.rb
+++ spec/features/users/user_can_log_in_spec.rb
@@ -17,4 +17,22 @@ RSpec.describe "Logging In" do
 
     expect(current_path).to eq(user_path(user1))
   end
+
+  it "can't log in with invalid credentials" do
+    user1 = User.create!(name: "Mike Smith", email: "msmith@gmail.com", password: "password1", password_confirmation: "password1")
+
+    visit root_path
+
+    click_on "Log in"
+
+    expect(current_path).to eq(login_path)
+
+    fill_in :email, with: user1.email
+    fill_in :password, with: "mike"
+
+    click_on "Log In"
+
+    expect(current_path).to eq(login_path)
+    expect(page).to have_content("Sorry, your credentials are bad.")
+  end
 end
