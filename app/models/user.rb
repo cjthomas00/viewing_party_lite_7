1e06f36fdebc245af6c3ac392b0b57f04f7ddf95
Caleb Thomas <cjthomas00@comcast.net>
2023-04-03 13:18:47 -0600

    add password_digest validations tests and validations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,10 @@ class User < ApplicationRecord
   has_many :viewing_party_users
   has_many :viewing_parties, through: :viewing_party_users
 
-  validates_presence_of :name, :email
+  validates_presence_of :name, :email, :password_digest
   validates_uniqueness_of :email
+  
+  has_secure_password
 
   def hosted_parties
     viewing_parties.where("host = true")
